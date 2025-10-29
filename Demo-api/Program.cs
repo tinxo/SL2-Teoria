b492a8c5ae2cf99c11bbfcc5982a7615b0af8cb4
Martin Rey <m.rey00@gmail.com>
2025-10-29 20:05:41 -0300

    Cambios para la API de DolarApi.com y hacer varias consultas a través de los tipos Task y los modificadores async y await.

--- Demo-api/Program.cs
+++ Demo-api/Program.cs
@@ -1,26 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Vamos a hacer una consulta a la API");
+﻿using System.Text.Json;
 
-string url = "https://dog.ceo/api/breed/mestizo/images/random";
-
-HttpClient client = new HttpClient();
-
-try
+class DolarResponse
 {
-    HttpResponseMessage response = await client.GetAsync(url);
-    if (response.IsSuccessStatusCode)
-    {
-        string responseBody = await response.Content.ReadAsStringAsync();
-        Console.WriteLine(responseBody);
-    }
-    else
-    {
-        Console.WriteLine($"Error: {response.StatusCode}");
-        // Console.WriteLine($"Error desc: {response}");
-    }
-    
+    public decimal venta { get; set; }
+    public decimal compra { get; set; }
+    public string casa { get; set; }
+    public string nombre { get; set; }
+    public DateTime fechaActualizacion { get; set; }
 }
-catch (HttpRequestException e)
+
+class ConsultaApi
 {
-    Console.WriteLine($"Request error: {e.Message}");    
+
+public static async Task ConsultaDolarApiAsync()
+    {
+        string urlBlue = "https://dolarapi.com/v1/dolares/blue";
+        string urlOficial = "https://dolarapi.com/v1/dolares/oficial";
+        string urlCripto = "https://dolarapi.com/v1/dolares/cripto";
+
+        HttpClient client = new HttpClient();
+
+        try
+        {
+            Console.WriteLine("Varias consultas");
+
+            Console.WriteLine("Consulta Blue");
+            Task<HttpResponseMessage> taskBlue = client.GetAsync(urlBlue);
+            Console.WriteLine("Consulta Oficial");
+            Task<HttpResponseMessage> taskOficial = client.GetAsync(urlOficial);
+            Console.WriteLine("Consulta Cripto");
+            Task<HttpResponseMessage> taskCripto = client.GetAsync(urlCripto);
+
+            HttpResponseMessage responseOficial = await taskOficial;
+            Console.WriteLine("Escritura async Oficial");
+            string respuesta = await responseOficial.Content.ReadAsStringAsync();
+            DolarResponse dolarOficial = JsonSerializer.Deserialize<DolarResponse>(respuesta);
+            Console.WriteLine($"Precio de venta: ${dolarOficial.venta}");
+
+            HttpResponseMessage responseBlue = await taskBlue;
+            Console.WriteLine("Escritura async Blue");
+            Console.WriteLine(await responseBlue.Content.ReadAsStringAsync());
+
+            HttpResponseMessage responseCripto = await taskCripto;
+            Console.WriteLine("Escritura async Cripto");
+            Console.WriteLine(await responseCripto.Content.ReadAsStringAsync());
+            
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Request error: {e.Message}");
+        }
+
+    }
+
+    public static void ConsultaDolarApi()
+    {
+        string url = "https://dolarapi.com/v1/dolares/blue";
+
+        HttpClient client = new HttpClient();
+
+        try
+        {
+            Console.WriteLine("Antes de hacer la consulta");
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            Console.WriteLine("Escritura no async");
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(responseBody);
+            }
+            else
+            {
+                Console.WriteLine($"Error en la consulta a la API:");
+                Console.WriteLine($"Status Code: {response.StatusCode}");
+                Console.WriteLine($"Mensaje: {response.ReasonPhrase}");
+            }
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"Request error: {e.Message}");
+        }
+
+    }
+
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Vamos a hacer una consulta a la API de DolarAPI.com");
+        await ConsultaDolarApiAsync();
+        Console.WriteLine("Consulta finalizada.");
+    }
 }
